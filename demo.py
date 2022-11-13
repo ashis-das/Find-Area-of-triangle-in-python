ef0466b8dd0f2bfcc0c16f20778132d342acadd1
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-13 20:13:55 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,7 @@
+# Find Area of triangle
+#take two user input number
+print("this area is valid for right angled triangle only")
+base=int(input("enter the length of the base"))
+height=int(input("enter the length of the height"))
+area=(1/2)*(base*height)
+print("the area of the triangle is", area)
